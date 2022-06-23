f1e58d0b1eb53f366b9a97e9c5ac917d37d9bcd9
Скубиш Владлена Борисовна <VBoSkubish@sberbank.ru>
2022-06-23 16:29:47 +0300

    p1 done

--- inception-of-things/p1/scripts/prepare_master_vm.sh
+++ inception-of-things/p1/scripts/prepare_master_vm.sh
@@ -14,7 +14,7 @@ k3s=$(which k3s)
 if [ -z "$k3s" ]
 then
     echo "[INFO] Install k3s and set $1 as server"
-    curl -sfL https://get.k3s.io | sh -s - server --node-ip $1
+    curl -sfL https://get.k3s.io | sh -s - server --node-ip $1 --tls-san $1
     # Problem: WARN[0014] Unable to read /etc/rancher/k3s/k3s.yaml, please start server with --write-kubeconfig-mode to modify kube config permissions 
     mkdir -p /home/vagrant/.kube # ignore command if directory exists
     sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config && \
@@ -24,3 +24,5 @@ then
         echo "alias k=\"k3s kubectl\"" >> /home/vagrant/.bashrc
 fi
 
+echo "[INFO] Copying server $1 token to host machine to create agents"
+sudo cp /var/lib/rancher/k3s/server/node-token /tmp/confs/server_token
--- inception-of-things/p1/scripts/prepare_slave_vm.sh
+++ inception-of-things/p1/scripts/prepare_slave_vm.sh
@@ -15,6 +15,13 @@ k3s=$(which k3s)
 if [ -z "$k3s" ]
 then
     echo "[INFO] Install k3s and set $1 as agent"
-    curl -sfL https://get.k3s.io | sh -s - --token-file /tmp/token \
+    curl -sfL https://get.k3s.io | K3S_URL=https://$2:$3 K3S_TOKEN_FILE=/tmp/confs/server_token sh -s - \
         --node-ip $1 # download k3s and set as server worker (or agent)
+    # Problem: WARN[0014] Unable to read /etc/rancher/k3s/k3s.yaml, please start server with --write-kubeconfig-mode to modify kube config permissions 
+    mkdir -p /home/vagrant/.kube # ignore command if directory exists
+    sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config && \
+        sudo chown vagrant /home/vagrant/.kube/config && \
+        sudo chmod 600 /home/vagrant/.kube/config && \
+        echo "export KUBECONFIG=/home/vagrant/.kube/config" >> /home/vagrant/.bashrc
+        echo "alias k=\"k3s kubectl\"" >> /home/vagrant/.bashrc
 fi
