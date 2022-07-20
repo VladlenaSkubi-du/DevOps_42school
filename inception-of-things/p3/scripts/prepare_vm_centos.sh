ba1920b30466a8f122242dd3a0cc1c0582164ef1
Скубиш Владлена Борисовна <VBoSkubish@sberbank.ru>
2022-07-20 22:29:48 +0300

    Nothing works on VM

--- inception-of-things/p3/scripts/prepare_vm_centos.sh
+++ inception-of-things/p3/scripts/prepare_vm_centos.sh
@@ -2,20 +2,41 @@
 # $ cat /etc/centos-release
 # CentOS Linux release 7.9.2009 (Core)
 
-echo "[INFO] Installing docker"
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo -y
-sudo yum install docker-ce -y
+echo "[INFO] Update packages"
+sudo yum update -y
+export PATH=/usr/local/bin:/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
+
+ifconfig=$(which ifconfig)
+if [ -z "$ifconfig" ]
+then
+    echo "[INFO] Install net-tools"
+    sudo yum install net-tools -y # download ifconfig
+fi
+
+ifconfig=$(which docker)
+if [ -z "$ifconfig" ]
+then
+    echo "[INFO] Installing docker"
+    sudo yum install -y yum-utils
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo -y
+    sudo yum install docker-ce docker-ce-cli containerd.io -y
+fi
+echo "[INFO] Starting docker"
 sudo systemctl start docker
 
-echo "[INFO] Installing k3d"
-sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo chmod +x kubectl
-echo "export PATH=$PATH:$HOME" >> $HOME/.bashrc
+ifconfig=$(which k3d)
+if [ -z "$ifconfig" ]
+then
+    echo "[INFO] Installing k3d"
+    sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+fi
 
-echo "[INFO] Starting the cluster with dev and argocd namespaces"
-k3d cluster create fullcdargocd
-kubectl create namespace argocd
-kubectl create namespace dev
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl port-forward svc/argocd-server 8080:443 -n argocd
+ifconfig=$(which kubectl)
+if [ -z "$ifconfig" ]
+then
+    echo "[INFO] Installing kubectl to the current directory"
+    sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo chmod +x kubectl
+    echo "export PATH=$PATH:$HOME" >> $HOME/.bashrc
+fi
+
