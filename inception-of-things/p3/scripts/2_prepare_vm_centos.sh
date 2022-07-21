3df78ddac8d7fb62a31f7559fd3375e367831ce9
Скубиш Владлена Борисовна <VBoSkubish@sberbank.ru>
2022-07-21 15:07:50 +0300

    Done on VM

--- inception-of-things/p3/scripts/2_prepare_vm_centos.sh
+++ inception-of-things/p3/scripts/2_prepare_vm_centos.sh
@@ -22,6 +22,7 @@ then
     sudo yum install docker-ce docker-ce-cli containerd.io -y
 fi
 echo "[INFO] Starting docker"
+sudo chmod 666 /var/run/docker.sock
 sudo systemctl start docker
 
 ifconfig=$(which k3d)
@@ -36,7 +37,6 @@ if [ -z "$ifconfig" ]
 then
     echo "[INFO] Installing kubectl to the current directory"
     sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-    sudo chmod +x kubectl
-    echo "export PATH=$PATH:$HOME" >> $HOME/.bashrc
+    sudo mv kubectl /usr/local/bin/
+    sudo chmod +x /usr/local/bin/kubectl
 fi
-
