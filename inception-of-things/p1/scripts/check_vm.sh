d73f0b24304cfd0d5bfd7a7216858beba8a32e6e
Скубиш Владлена Борисовна <VBoSkubish@sberbank.ru>
2022-07-22 00:20:30 +0300

    Delete unused scripts

--- inception-of-things/p1/scripts/check_vm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "VM is connected to private network as an internal network for VirtualBox with address inet 192.168.42.110"
-ifconfig eth1
-
-echo "Checking nodes of k3s"
-k get nodes -o wide
