f101b5662585088d277cb0471f9340dbc16db7d4
Скубиш Владлена Борисовна <VBoSkubish@sberbank.ru>
2022-07-22 20:38:57 +0300

    Help done

---
+++ inception-of-things/p3/scripts/create_cluster_with_argocd.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# k3d: https://k3d.io/v5.4.1/usage/exposing_services/
+# argocd: https://argo-cd.readthedocs.io/en/stable/getting_started/
+
+NAME="argocd_application"
+
+echo "Create k3d cluster with mounting 8888 of docker container to 8888 host port"
+k3d cluster create $NAME -p "8888:8888@loadbalancer"
+
+echo "Create namespaces"
+kubectl create namespace argocd
+kubectl create namespace dev
+
+echo "Create argocd agent"
+kubectl apply -n argocd -f confs/argocd_install.yml
+while ! kubectl wait -n argocd --for=condition=Ready pods --all; do; done
+
+echo "Make port-forwarding in other terminal to get argocd server: kubectl port-forward svc/argocd-server -n argocd 4242:443"
+echo "Password to enter argocd server UI"
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+
+echo "Create application"
+kubectl apply -n argocd -f confs/argocd_application.yml
+while ! kubectl wait -n dev --for=condition=Ready pods --all; do; done
